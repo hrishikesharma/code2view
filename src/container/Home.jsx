6d84ce96139971c84e7745d2bc8277adb6e5edc5
Hrishikesh Sarma <hsarma414@gmail.com>
2023-10-18 01:47:33 +0530

    added signup button

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -62,13 +62,25 @@ function Home() {
         {/* Top section */}
         <div className="w-full flex items-center justify-between gap-3">
             {/* Search */}
-            <div>
+            <div className="bg-secondary w-full px-4 py-3 rounded-md flex
+            items-center justify-center gap-3">
                 <FaSearchengin className="text-2xl text-primaryText"/>
                 <input type="text" className="flex-1 px-4 py-1 text-xl bg-transparent outline-none border-none text-primaryText
                 placeholder:text-gray-600" placeholder="Search here..." />
             </div>
-
             {/* Profile section */}
+            {!user && (
+                <motion.div whileTap={{scale : 0.9}} className="flex items-center justify-center gap-3">
+                  <Link to={"/home/auth"} className="bg-emerald-500 px-6 py-2
+                  rounded-md text-white text-lg cursor-pointer hover:bg-emerald-700">
+                        SignUp
+                  </Link>
+                </motion.div>
+            )}
+
+            {user && (
+                <div></div>
+            )}
         </div>
       </div>
     </>
